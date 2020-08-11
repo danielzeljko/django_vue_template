e78c4adb85cd6f41ac544e2c91d175822491e789
Daniel Zeljko <danielatupwork@gmail.com>
2020-08-11 06:36:28 -0700

    test aws s3

--- django_vue_template/settings.py
+++ django_vue_template/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-AWS_STORAGE_BUCKET_NAME = 'django-ruby-static-2'
-AWS_ACCESS_KEY_ID = 'AKIAJ3AJM5NAOSDSU62Q'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_S3_CUSTOM_DOMAIN = 'django-ruby-static-2.s3.amazonaws.com'
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_CUSTOM_DOMAIN = os.environ['AWS_S3_CUSTOM_DOMAIN']
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://django-ruby-static-2.s3.amazonaws.com/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATIC_URL = os.environ['STATIC_URL']
 
 # Application definition
 
